87750045c9fbf71a3c478709173e4a5367abc1de
vishnukr2425 <vishnukr2425@gmail.com>
2023-04-09 16:59:21 +0530

    final

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const hbs= require("hbs");
 const port = process.env.PORT || 8000;
 require("./db/conn.js");
 const Register= require("./models/registers");
+const Outpass_data=require("./models/outpass_data.js");
 
 const staticpath= path.join(__dirname, "../public");
 const viewspath= path.join(__dirname, "../template/views");
@@ -58,6 +59,43 @@ app.post("/register", async(req, res)=>{
      else{
          res.send("Passwords are not matching");
      }
+    } catch (error) {
+     res.status(400).send(error);
+    }
+ })
+
+ //login validation
+app.post("/login", async(req, res)=>{
+    try {
+     const email=req.body.email;
+     const pswd=req.body.pswd;
+     const useremail= await Register.findOne({email:email});
+    
+     if(useremail.password===pswd){
+        res.status(201).render("apply");
+     }else{
+        res.send("password is not matching... For any query contact Saurabh Rangdale")
+     }
+    } catch (error) {
+     res.status(400).send("Invalid Email");
+    }
+ })
+
+ //Apply for outpass online
+ app.post("/apply", async(req, res)=>{
+    try {
+     const outpass=new Outpass_data({
+        name: req.body.name,
+        roll: req.body.roll,
+        hostel: req.body.hostel,
+        room: req.body.room,
+        phone: req.body.phone,
+        departure: req.body.departure,
+        return: req.body.return,
+        reason: req.body.reason
+     })
+     const applied=await outpass.save();
+     res.status(201).render("confirm");
     } catch (error) {
      res.status(400).send(error);
     }
